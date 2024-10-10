5d12e79f9a9d6b1b3345aabd40ad50dbcfd17a4c
tamilatquest <tamil@quest1.io>
2024-10-10 16:33:38 +0530

    ui design change and logo added

---
+++ gammalighttherapy/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  gammalighttherapy
+//
+//  Created by Tamilarasan on 10/10/24.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
